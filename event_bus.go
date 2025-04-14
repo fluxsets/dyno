c54cff924ca0117de641f8a20286041a8ad10517
Qiulin Li <liqiulin@outlook.com>
2025-04-14 20:15:01 +0800

    update: eventbus

--- event_bus.go
+++ event_bus.go
@@ -1,4 +1,23 @@
 package dyno
 
+import "gocloud.dev/pubsub"
+
 type EventBus interface {
+	Subscription(topic string) *pubsub.Subscription
+	Topic(topic string) *pubsub.Topic
+}
+
+type pubSubEventBus struct {
+}
+
+func (pb *pubSubEventBus) Subscription(topic string) *pubsub.Subscription {
+}
+
+func (pb *pubSubEventBus) Topic(topic string) *pubsub.Topic {
+}
+
+var _ EventBus = (*pubSubEventBus)(nil)
+
+func NewEventBus() EventBus {
+	return &pubSubEventBus{}
 }
--- eventbus/subscriber.go
+++ eventbus/subscriber.go
@@ -3,8 +3,11 @@ package eventbus
 import (
 	"context"
 	"github.com/fluxsets/dyno"
+	"gocloud.dev/pubsub"
 	"strings"
 )
+
+type HandlerFunc func(ctx context.Context, msg *pubsub.Message) error
 
 const TopicSep = "::"
 
@@ -26,26 +29,51 @@ func (t TopicURI) EventName() string {
 }
 
 type Subscriber struct {
-	topic TopicURI
+	topic   TopicURI
+	dyno    dyno.Dyno
+	handler HandlerFunc
+	subs    *pubsub.Subscription
 }
 
-func (s Subscriber) ID() string {
+func NewSubscriberProducer(topic TopicURI, h HandlerFunc) dyno.DeploymentProducer {
+	return func() dyno.Deployment {
+		return &Subscriber{
+			topic:   topic,
+			handler: h,
+		}
+	}
+}
+
+func (s *Subscriber) ID() string {
 	return "subscriber:" + s.topic.String()
 }
 
-func (s Subscriber) Init(do dyno.Dyno) error {
-	//TODO implement me
-	panic("implement me")
+func (s *Subscriber) Init(do dyno.Dyno) error {
+	s.dyno = do
+	s.subs = s.dyno.EventBus().Subscription(s.topic.EventName())
+	return nil
 }
 
-func (s Subscriber) Start(ctx context.Context) error {
-	//TODO implement me
-	panic("implement me")
+func (s *Subscriber) Start(ctx context.Context) error {
+	var err error
+	for {
+		var msg *pubsub.Message
+		msg, err = s.subs.Receive(ctx)
+		if err != nil {
+			break
+		}
+		if err := s.handler(ctx, msg); err != nil {
+			s.dyno.Logger().Error("message handle error", "topic", s.topic, "error", err)
+		}
+		msg.Ack()
+	}
+	return err
 }
 
-func (s Subscriber) Stop(ctx context.Context) {
-	//TODO implement me
-	panic("implement me")
+func (s *Subscriber) Stop(ctx context.Context) {
+	if err := s.subs.Shutdown(ctx); err != nil {
+		s.dyno.Logger().Error("subscription shutdown error", "topic", s.topic, "error", err)
+	}
 }
 
 var _ dyno.ServerLike = new(Subscriber)
